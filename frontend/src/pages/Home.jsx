a2e4fee7d755c18a0eac3646088f6797048985db
swara <swara.kubade@gmail.com>
2025-02-11 14:20:05 +0530

    Displayed the list of books in the table and added icons for other operations

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,6 +31,46 @@ const Home = () => {
              <MdOutlineAddBox className='text-sky-400 text-4xl' />
             </Link>
         </div>
+        {loading ? (
+            <Spinner />
+        ) : (
+            <table className='w-full table-auto  rounded-tl-lg rounded-tr-lg border-separate border-spacing-2'>
+            <thead>
+                <tr>
+                    <th className='border-border-slate-600 rounded-md'>No</th>
+                    <th className='border-border-slate-600 rounded-md'>Title</th>
+                    <th className='border-border-slate-600 rounded-md'>Author</th>
+                    <th className='border-border-slate-600 rounded-md'>Publish Year</th>
+                    <th className='border-border-slate-600 rounded-md'>Genre</th>
+                    <th className='border-border-slate-600 rounded-md'>Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                {books.map((book, index) => (
+                    <tr key={book._id} className='h-8'>
+                        <td className='border border-slate-700 rounded-md text-center'>{index + 1}</td>
+                        
+                        <td className='border border-slate-700 rounded-md text-center'>{book.title}</td>
+
+                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.author}</td>
+
+                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.publishYear}</td>
+
+                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.genre}</td>
+
+                        <td className='border border-slate-700 rounded-md text-center'>
+                            <div className='flex justify-center gap-x-4'>
+                           <Link to={`/books/details/${book._id}`}>
+                            <BsInfoCircle className='text-sky-400 text-2xl' /></Link>
+                            <Link to={`/books/edit/${book._id}`}><AiOutlineEdit className='text-2xl text-yellow-600'/>
+                            </Link>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        )}
     </div>
   )
 }
